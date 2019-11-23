9b151c0e615a727e4c85fb306056a69c4410056b
Steve Price <steveprice88+anon@gmail.com>
2019-11-23 22:41:56 +0000

    Updating Readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# inventory-management
+# Inventory Manangement
+
+## Dependancies
+
+All packages are managed by Nuget Package Manager and will restore on build.  
+  
+xUnit  
+AutoFac  
+FluentAssertions
+
+## Usage
+
+Build, Run and Test in Visual Studio 2017. Web Application will launch using IIS Express.  
+ Alternatively from Project Root run the following in command ine
+
+```console
+dotnet test
+dotnet run --project src/InventoryManagement.csproj
+```
+
+## Further Improvements
+Break out Rules into seperate project  
+Seperate Dependancy Injection into seperate Module  
+BDD style tests
+
---
+++ tests/UnitTests/InventoryManagementServiceUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using InventoryManagement.Models;
+using InventoryManagement.Rules;
+using Xunit;
+
+namespace InventoryManagement.Tests.UnitTests
+{
+    public class InventoryManagementServiceUnitTest
+    {
+        [Fact]
+        public void GivenSoap_WhenUpdated_SellinStaysTheSame()
+        {
+
+            var item = new Item() {Name = "Soap", Quality = 2, SellIn = 2};
+            var rule = new SoapUpdateRule();
+            rule.Update(item);
+
+            item.SellIn.Should().Be(2);
+        }
+
+        [Fact]
+        public void GivenSoap_WhenUpdated_QualityStaysTheSame()
+        {
+
+            var item = new Item() { Name = "Soap", Quality = 2, SellIn = 2 };
+            var rule = new SoapUpdateRule();
+            rule.Update(item);
+
+            item.Quality.Should().Be(2);
+        }
+
+
+    }
+}
